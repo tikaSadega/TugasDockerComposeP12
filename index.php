a1b074957cdfbd7bbff6e97a9b40d405ef728597
yovinsilaban <32691142+yovinsilaban@users.noreply.github.com>
2017-12-24 23:59:40 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>155410069</title>
+</head>
+<body>
+	<h2>Tugas Teori TCC Pertemuan 12 Docker Compose</h2>
+	<h5><br>Nama    : Yovin Silaban</br></h5>
+	<h5>Nim		: 155410069</h5>
+	<h5>Git Hub : https://github.com/yovinsilaban</h5>
+	
+</body>
+</html>
